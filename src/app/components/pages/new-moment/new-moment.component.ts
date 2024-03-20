f1b489809152cb94423337dcceb485d495c4d076
Alexandre Junior <alejunior.dev@gmail.com>
2024-03-20 18:29:28 -0300

    feat: finish system message

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,41 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Moment } from 'src/app/Moment';
+import { MessagesService } from 'src/app/services/messages.service';
 import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+  styleUrls: ['./new-moment.component.css'],
 })
 export class NewMomentComponent implements OnInit {
+  btnText = 'Compartilhar';
 
-  btnText = "Compartilhar"
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router
+  ) {}
 
-  constructor(private momentService: MomentService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
 
-  async createHandler(moment: Moment){
-    const formData = new FormData()
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
 
-    formData.append('title', moment.title)
-    formData.append('description', moment.description)
-
-    if(moment.image){
-      formData.append('image', moment.image)
+    if (moment.image) {
+      formData.append('image', moment.image);
     }
 
     // to-do
-    await this.momentService.createMoment(formData).subscribe()
-
-
-    // enviar para o service
+    await this.momentService.createMoment(formData).subscribe();
 
     // exibir mensagem
+    this.messagesService.add('Momento criado com sucesso!');
 
     /// redirect
+    this.router.navigate(['/'])
   }
-
 }
